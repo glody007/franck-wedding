6ea5506321b66c159668ea24bd4d05b64ba43600
glody007 <glodymbutwile@gmail.com>
2022-07-06 14:45:13 +0200

    feat: Invitation details

--- src/client/js/invitation.js
+++ src/client/js/invitation.js
@@ -1,9 +1,13 @@
+
+const DETAILS = 'Details'
+const INVITATION = 'Invitation'
+
 /**
- * Get data from api
+ * Get invitation data and add set name and table
  * @param {String} url 
  */
- const getData = async (url) => {
-    const response = await fetch(url, {
+ const getInvitationData = async () => {
+    const response = await fetch('/invitations/datas/franck', {
         method: 'GET',
         credentials: 'same-origin',
         headers: {
@@ -12,10 +16,44 @@
     })
     try {
         const data = await response.json()
-        console.log(data)
+        const name = document.getElementById('name')
+        const table = document.getElementById('table')
+        name.innerText = data['name']
+        table.innerText = data['table']
     } catch (error) {
         console.log(error.data)
     }
 }
 
-getData('/invitations/datas/franck')
+/**
+ * Change text and color of button
+ * @param {HtmlElement} button 
+ */
+const changeButtonDetails = (button) => {
+    if(button.innerText === DETAILS) {
+        button.innerText = INVITATION
+        button.style.backgroundColor =  "#f59042"
+    } else {
+        button.innerText = DETAILS
+        button.style.backgroundColor =  "white"
+    }
+}
+
+/**
+ * Add click listener to button, for showing or hidding details
+ */
+const addDetailsListener = () => {
+    const button = document.getElementById('details-button')
+    button.addEventListener('click', () => {
+        const detailsContent = document.getElementById('details-content')
+        const mainContent = document.getElementById('main-content')
+        detailsContent.classList.toggle('hide')
+        mainContent.classList.toggle('hide')
+        changeButtonDetails(button)
+    })
+}
+
+(() => {
+    getInvitationData()
+    addDetailsListener()
+})()
