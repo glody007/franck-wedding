eb610b2d3f27e511f266874b417dfebc7f835eb8
glody007 <glodymbutwile@gmail.com>
2022-07-06 17:20:39 +0200

    feat: first and second tables

--- src/server/mockApi.js
+++ src/server/mockApi.js
@@ -1,8 +1,48 @@
 const invitations = {
-    franck: {
-        name: 'franck',
+    yannickjudith: {
+        name: 'Couple Yannick Mwamba & Judith',
         table: 'First'
-    }
+    },
+    lucienberthe: {
+        name: 'Couple Lucien Mwamba & Berthe',
+        table: 'First'
+    },
+    nonolydie: {
+        name: 'Couple Nono Mutombo',
+        table: 'First'
+    },
+    mubakemechi: {
+        name: 'Mubakemechi',
+        table: 'First'
+    },
+    mireillewayne: {
+        name: 'Couple Mireille Mwamba & Wayne',
+        table: 'First'
+    },
+    dannynelly: {
+        name: 'Couple Danny & Nelly',
+        table: 'Second'
+    },
+    yannickgrace: {
+        name: 'Couple Yannick Kakama',
+        table: 'Second'
+    },
+    audreylubenga: {
+        name: 'Couple Audrey lubenga',
+        table: 'Second'
+    },
+    pasteurjoel: {
+        name: 'Couple Pasteur Joel ',
+        table: 'Second'
+    },
+    kalinda: {
+        name: 'Pasteur Kalinda',
+        table: 'Second'
+    },
+    bengroupon: {
+        name: 'Ben Groupon',
+        table: 'Second'
+    },
 }
 
 module.exports = invitations
