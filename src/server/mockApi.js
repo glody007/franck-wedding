26cf10e0e007bdc8bc4c2ab354fb3a000d52afc2
glody007 <glodymbutwile@gmail.com>
2022-07-08 07:44:12 +0200

    feat: all invitations added

--- src/server/mockApi.js
+++ src/server/mockApi.js
@@ -1,4 +1,5 @@
 const invitations = {
+    // First Table
     yannickjudith: {
         name: 'Couple Yannick Mwamba & Judith',
         table: 'First'
@@ -19,6 +20,7 @@ const invitations = {
         name: 'Couple Mireille Mwamba & Wayne',
         table: 'First'
     },
+    // Second Table
     dannynelly: {
         name: 'Couple Danny & Nelly',
         table: 'Second'
@@ -42,6 +44,182 @@ const invitations = {
     bengroupon: {
         name: 'Ben Groupon',
         table: 'Second'
+    },
+    // Third Table
+    mellykashinda: {
+        name: 'Couple Melly Kashinda',
+        table: '3th'
+    },
+    nathanmbombo: {
+        name: 'Couple Nathan Mbombo',
+        table: '3th'
+    },
+    tharecmutale: {
+        name: 'Tharec Mutale',
+        table: '3th'
+    },
+    elisekabuya: {
+        name: 'Elizé Kabuya',
+        table: '3th'
+    },
+    bernardyumba: {
+        name: 'Bernard yumba',
+        table: '3th'
+    },
+    jaelmutombo: {
+        name: 'Jael Mutombo',
+        table: '3th'
+    },
+    henry: {
+        name: 'Henry',
+        table: '3th'
+    },
+     // Fourth Table
+     elogengoy: {
+        name: 'Eloge Ngoy',
+        table: '4th'
+    },
+    didierkashala: {
+        name: 'Didier Kashala',
+        table: '4th'
+    },
+    christianmukuna: {
+        name: 'Christian Mukuna',
+        table: '4th'
+    },
+    auriolmutundele: {
+        name: 'Auriol Mutundele',
+        table: '4th'
+    },
+    fabienbeya: {
+        name: 'Fabien Beya',
+        table: '4th'
+    },
+    marcboss: {
+        name: 'Marc Boss',
+        table: '4th'
+    },
+    linotl: {
+        name: 'Lino TL',
+        table: '4th'
+    },
+    dominiquekiboko: {
+        name: 'Dominique kiboko',
+        table: '4th'
+    },
+    kevinhkibari: {
+        name: 'Kevinh kibari',
+        table: '4th'
+    },
+    titekabwe: {
+        name: 'Tite Kabwe',
+        table: '4th'
+    },
+    // Fifth Table
+    robertmwamba: {
+        name: 'Couple Robert Mwamba',
+        table: '5th'
+    },
+    pasteurjose: {
+        name: 'Couple pasteur Jose',
+        table: '5th'
+    },
+    mamanalpho: {
+        name: 'Maman Alpho',
+        table: '5th'
+    },
+    louiseasumini: {
+        name: 'Maman Louise Asumini',
+        table: '5th'
+    },
+    papaevaristemwamba: {
+        name: 'Papa Evariste Mwamba',
+        table: '5th'
+    },
+    // Sixth Table
+    erictshitenda: {
+        name: 'Couple Eric Tshitenda',
+        table: ''
+    },
+    docteurfelilukusa: {
+        name: 'Couple Docteur Feli Lukusa',
+        table: ''
+    },
+    banzajaguer: {
+        name: 'Couple Banza Jaguer',
+        table: ''
+    },
+    pasteurcorneilleaudille: {
+        name: 'Couple Pasteur Corneille Mutombo & Audille',
+        table: ''
+    },
+    // - Table
+    sabrinamutatche: {
+        name: 'Sabrina Mutatche',
+        table: ''
+    },
+    // -- Table
+    djokapambu: {
+        name: 'Djo Kapambu',
+        table: ''
+    },
+    guereroeddy: {
+        name: 'Guerero Eddy',
+        table: ''
+    },
+    deborahwakutemba: {
+        name: 'Deborah Wakutemba',
+        table: ''
+    },
+    achildjlux: {
+        name: 'Achil Dj Lux',
+        table: ''
+    },
+    // --- Table
+    yveskashila: {
+        name: 'Yves Kashila',
+        table: ''
+    },
+    sergemwamba: {
+        name: 'Serge Mwamba',
+        table: ''
+    },
+    paulkabasele: {
+        name: 'Paul Kabasele',
+        table: ''
+    },
+    michaeldjamba: {
+        name: 'Michael Djamba',
+        table: ''
+    },
+    louisonchristine: {
+        name: 'Couple Louison Tunasi & Christine',
+        table: ''
+    },
+    louisonforward: {
+        name: 'Louison Forward',
+        table: ''
+    },
+    angemalaika: {
+        name: 'Ange Malaika',
+        table: ''
+    },
+    kyriademba: {
+        name: 'Couple Kyria Demba',
+        table: ''
+    },
+    marcokisimba: {
+        name: 'Marco Kisimba',
+        table: ''
+    },
+    // ---- Table
+    thierryfilsmwamba: {
+        name: 'Thierry - Fils Mwamba',
+        table: ''
+    },
+    jonathanmulumba: {
+        name: 'Jonathan Mulumba',
+        table: ''
     },
 }
 
